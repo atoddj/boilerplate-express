24eeff3aa0afe6c731adec24ef140fd85dea331f
Todd Jones <atoddj@gmail.com>
2021-06-18 12:09:48 -0400

    added json response

--- myApp.js
+++ myApp.js
@@ -4,11 +4,15 @@ console.log('Hello World');
 
 var path = __dirname + '/views/index.html';
 
+app.use('/public', express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
     res.sendFile(path);
 });
 
-app.use('/public', express.static(__dirname + '/public'));
+app.get('/json', (req, res) => {
+    res.json({"message": "Hello json"});
+})
 
 
 
