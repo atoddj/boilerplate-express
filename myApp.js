747662f7e0863ac55ce19ec568e5f2f10bf40565
Todd Jones <atoddj@gmail.com>
2021-06-18 14:09:24 -0400

    added bodyParser to project

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
-console.log('Hello World');
+var bodyParser = require('body-parser');
 
 var path = __dirname + '/views/index.html';
 
+app.use(bodyParser.urlencoded({extended:false}));
 app.use('/public', express.static(__dirname + '/public'));
 app.use((req, res, next) => {
     //logger function
