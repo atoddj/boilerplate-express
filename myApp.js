6f1fc11192ae8b0f094efb0c468455b5b28569f4
Todd Jones <atoddj@gmail.com>
2021-06-18 11:58:01 -0400

    updated to sendFile

--- myApp.js
+++ myApp.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 console.log('Hello World');
 
-var path = __dirname + 'views/index.html';
+var path = __dirname + '/views/index.html';
 
 app.get('/', function(req, res) {
-    res.send(path);
+    res.sendFile(path);
 });
 
 
